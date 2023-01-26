cb71166026207f33f4bb137c7f216b4e0fad6fcf
kevin170703 <correakevinfabian01@gmail.com>
2023-01-26 11:55:02 -0300

    seguridad de url de DB

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-import { passwors } from "./password";
-const conectionString = `mongodb+srv://kevin03:${passwors}@cluster0.f2mukz7.mongodb.net/porducts?retryWrites=true&w=majority`;
+import { db } from "./datadb";
+const conectionString = db;
 
 // conexion a mongo
 mongoose
