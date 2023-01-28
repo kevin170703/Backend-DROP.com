a5002027250dd8b0e3ab4135beb5bd9975c2e02e
kevin170703 <correakevinfabian01@gmail.com>
2023-01-27 21:55:16 -0300

    rutas para las funciones

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 
 const router = Router();
-import { getProducts } from "../controllers/products";
+import {
+  getProducts,
+  createProduct,
+  getProductsByName,
+  deleteProduct,
+} from "../controllers/products";
 
 router.get("/products", getProducts);
+router.get("/products/:name", getProductsByName);
+router.post("/products", createProduct);
+router.delete("/products/:id", deleteProduct);
 
 export default router;
