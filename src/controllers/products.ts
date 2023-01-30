1dc2687172231f9060c189fa94b69b57f29c817c
kevin170703 <correakevinfabian01@gmail.com>
2023-01-30 19:26:21 -0300

    actulizacion de pedidos de productos

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,80 +1,94 @@
 import Product from "../models/Product";
-import { Request, Response, NextFunction } from "express";
+import User from "../models/User";
+import { Request, Response } from "express";
 
-export const getProducts = (_req: Request, res: Response) => {
-  Product.find({})
-    .then((result) => {
-      if (!result.length)
-        return res.json({ msj: "La base de datos se ecuentra vacia" });
-      return res.json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(404).end();
+export const getProducts = async (_req: Request, res: Response) => {
+  try {
+    const product = await Product.find({}).populate("user", {
+      userName: 1,
+      email: 1,
     });
+
+    if (!product.length)
+      return res.json({ msj: "La base de datos se ecuentra vacia" });
+
+    return res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(404).end();
+  }
 };
 
-export const getProductsByName = (req: Request, res: Response) => {
+export const getProductsByName = async (req: Request, res: Response) => {
   const { name } = req.params;
-  Product.find({ nameProduct: name.toLowerCase() })
-    .then((product) => {
-      if (!product.length)
-        res.status(404).send({
-          error: "El nombre de el producto introducido es incorrecto",
-        });
-      return res.json(product);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(404).end();
-    });
+  try {
+    const product = await Product.find({ nameProduct: name.toLowerCase() });
+
+    if (!product.length)
+      res.status(404).send({
+        error: "El nombre de el producto introducido es incorrecto",
+      });
+
+    return res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(404).end();
+  }
 };
 
-export const createProduct = (req: Request, res: Response) => {
-  const { nameProduct, stock, price } = req.body;
+export const createProduct = async (req: Request, res: Response) => {
+  const { nameProduct, stock, price, userId } = req.body;
+  const user = await User.findById(userId);
+
   const product = new Product({
     nameProduct: nameProduct,
     stock: stock,
     price: price,
+    user: user?._id,
   });
 
-  product
-    .save()
-    .then(() => {
-      res.send("El producto se creo correctamente");
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(404).end();
-    });
+  try {
+    const savedProduct = await product.save();
+
+    if (user !== null) {
+      user.products = user.products.concat(savedProduct._id);
+      await user.save();
+    }
+
+    res.send("El producto se creo correctamente");
+  } catch (error) {
+    console.log(error);
+    res.status(404).end();
+  }
 };
 
 export const updataProduct = (req: Request, res: Response) => {
   const { id } = req.params;
-  const product = req.body;
+  const { name, stock, price } = req.body;
   const newProductInfo = {
-    nameProduct: product.name,
-    stock: product.stock,
-    price: product.price,
+    nameProduct: name,
+    stock: stock,
+    price: price,
   };
-  Product.findByIdAndUpdate(id, newProductInfo, { new: true })
-    .then((product) => {
-      res.json(product);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(404).end();
+  try {
+    const product = Product.findByIdAndUpdate(id, newProductInfo, {
+      new: true,
     });
+
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(404).end();
+  }
 };
 
-export const deleteProduct = (req: Request, res: Response) => {
+export const deleteProduct = async (req: Request, res: Response) => {
   const { id } = req.body;
-  Product.findByIdAndDelete(id)
-    .then(() => {
-      res.send(`El producto con la id ${id} fue eliminado correctamente`);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(404).end();
-    });
+  try {
+    await Product.findByIdAndDelete(id);
+    res.send(`El producto con la id ${id} fue eliminado correctamente`);
+  } catch (error) {
+    console.log(error);
+    res.status(404).end();
+  }
 };
