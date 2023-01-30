03555536c9c8131708ca35fea20b02be6e732d0d
kevin170703 <correakevinfabian01@gmail.com>
2023-01-30 10:21:35 -0300

    middleware ruta no encontrada

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import "./db";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 const app = express();
 import routes from "./routes/routes";
+import notFound from "./middleware/notFound";
 
 const PORT = 3000;
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use(notFound);
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
