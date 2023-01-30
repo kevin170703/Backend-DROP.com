a3c722cadfc5f3b79ff889918b9da0a71a011955
kevin170703 <correakevinfabian01@gmail.com>
2023-01-30 19:28:45 -0300

    rutas de users y productos añadidas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,9 +8,14 @@ import {
   deleteProduct,
 } from "../controllers/products";
 
+import { newUser, getUsers } from "../controllers/users";
+
 router.get("/products", getProducts);
 router.get("/products/:name", getProductsByName);
 router.post("/products", createProduct);
 router.delete("/products/:id", deleteProduct);
+
+router.get("/users", getUsers);
+router.post("/users/create", newUser);
 
 export default router;
