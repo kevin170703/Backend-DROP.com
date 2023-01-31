066d1d911bcff38736a4c844ca818417bd8b5590
kevin170703 <correakevinfabian01@gmail.com>
2023-01-31 15:24:25 -0300

    añadiendo .env

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
-import { db } from "./datadb";
-const conectionString = db;
+const conectionString = process.env.DB_DATA;
 
 // conexion a mongo
-mongoose
-  .connect(conectionString)
-  .then(() => {
-    console.log("databse ok");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+if (conectionString) {
+  mongoose
+    .connect(conectionString)
+    .then(() => {
+      console.log("databse ok");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+} else console.log("conectionString invalido");
