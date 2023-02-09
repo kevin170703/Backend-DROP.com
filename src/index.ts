9dcadf0fe7616e7896081a45238137eeea840a33
kevin170703 <correakevinfabian01@gmail.com>
2023-02-09 11:01:45 -0300

    añadiendo cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import "dotenv/config";
 import "./db";
+import cors from "cors";
 import express from "express";
 const app = express();
 import routes from "./routes/routes";
 import notFound from "./middleware/notFound";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // middlwwares
 app.use(express.json());
+app.use(cors());
 
 // rutas
 app.use(routes);
