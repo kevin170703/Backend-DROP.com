49bc15035c27b0f2bc6565a3a069925fbd87678c
kevin170703 <correakevinfabian01@gmail.com>
2023-03-09 21:15:47 -0300

    cambiando a postgress y sequelize

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,27 +1,27 @@
 import { Router } from "express";
 const router = Router();
-import {
-  getProducts,
-  createProduct,
-  getProductsByName,
-  deleteProduct,
-  getProductsByIdUser,
-} from "../controllers/products";
+// import {
+//   getProducts,
+//   createProduct,
+//   getProductsByName,
+//   deleteProduct,
+//   getProductsByIdUser,
+// } from "../controllers/products";
 import { newUser, getUsers } from "../controllers/users";
-import { login } from "../controllers/login";
+// import { login } from "../controllers/login";
 
 // rutas de productos
-router.get("/products", getProducts);
-router.get("/products/:name", getProductsByName);
-router.get("/productsByIdUser/:id", getProductsByIdUser);
-router.post("/products", createProduct);
-router.delete("/products/:id", deleteProduct);
+// router.get("/products", getProducts);
+// router.get("/products/:name", getProductsByName);
+// router.get("/productsByIdUser/:id", getProductsByIdUser);
+// router.post("/products", createProduct);
+// router.delete("/products/:id", deleteProduct);
 
-// rutas de usuarios
+// // rutas de usuarios
 router.get("/users", getUsers);
 router.post("/users/create", newUser);
 
 // rutas de inicio de sesion
-router.post("/login", login);
+// router.post("/login", login);
 
 export default router;
