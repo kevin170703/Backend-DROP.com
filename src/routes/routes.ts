02d6e5346cd152b3e7aa776e2825851d6d505c14
kevin170703 <correakevinfabian01@gmail.com>
2023-03-11 10:38:35 -0300

    mudando a postgres y sequelize

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,21 +1,21 @@
 import { Router } from "express";
 const router = Router();
-// import {
-//   getProducts,
-//   createProduct,
-//   getProductsByName,
-//   deleteProduct,
-//   getProductsByIdUser,
-// } from "../controllers/products";
+import {
+  getProducts,
+  getProductsByName,
+  createProduct,
+  deleteProduct,
+  getProductsByIdUser,
+} from "../controllers/products";
 import { newUser, getUsers } from "../controllers/users";
 // import { login } from "../controllers/login";
 
 // rutas de productos
-// router.get("/products", getProducts);
-// router.get("/products/:name", getProductsByName);
-// router.get("/productsByIdUser/:id", getProductsByIdUser);
-// router.post("/products", createProduct);
-// router.delete("/products/:id", deleteProduct);
+router.get("/products", getProducts);
+router.get("/productsByIdUser/:id", getProductsByIdUser);
+router.get("/products/:name", getProductsByName);
+router.post("/products", createProduct);
+router.delete("/products/:id", deleteProduct);
 
 // // rutas de usuarios
 router.get("/users", getUsers);
