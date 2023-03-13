f078f8a9a6e2a14447da1fca85561bdfa09a8be2
kevin170703 <correakevinfabian01@gmail.com>
2023-03-13 17:30:52 -0300

    migrando a postgres y sequelize

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,27 +1,33 @@
 import { Response, Request } from "express";
-import User from "../models/User";
+import { User } from "../models/User";
 import bcrypt from "bcrypt";
 
-// export const login = async (req: Request, res: Response) => {
-//   const { userName, password } = req.body;
-//   const user = await User.findOne({
-//     where: {
-//       name: userName,
-//     },
-//   });
+export const login = async (req: Request, res: Response) => {
+  const { userName, password } = req.body;
+  if (!userName || !password)
+    return res
+      .status(401)
+      .send({ error: "El usuario o contraseña incorrecto" });
 
-// const passwordCorrect =
-//   user === null ? false : await bcrypt.compare(password, user.password);
+  const user = await User.findOne({
+    where: {
+      userName: userName,
+    },
+  });
 
-// if (!passwordCorrect || user === null)
-//   return res
-//     .status(401)
-//     .send({ error: "El usuario o contraseña incorrecto" });
+  const passwordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.password);
 
-// res.json({
-//   id: user.id,
-//   userName: user.userName,
-//   email: user.email,
-//   products: user.products,
-// });
-// };
+  if (!passwordCorrect || user === null) {
+    return res
+      .status(401)
+      .send({ error: "El usuario o contraseña incorrecto" });
+  }
+
+  res.json({
+    id: user.id,
+    userName: user.userName,
+    email: user.email,
+    products: user.products,
+  });
+};
