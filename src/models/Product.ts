b2747c06dd0c2edf6875eb67065f4c41add29920
kevin170703 <correakevinfabian01@gmail.com>
2023-03-13 17:31:39 -0300

    migrando a postgres y sequelize

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,25 +1,46 @@
-import { DataTypes } from "sequelize";
+import {
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  DataTypes,
+  CreationOptional,
+} from "sequelize";
 import sequelize from "../db";
 
-const Product = sequelize.define("Product", {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    allowNull: false,
-    primaryKey: true,
-  },
-  nameProduct: {
-    type: DataTypes.STRING,
-  },
-  stock: {
-    type: DataTypes.FLOAT,
-  },
-  price: {
-    type: DataTypes.FLOAT,
-  },
-  user: {
-    type: DataTypes.STRING,
-  },
-});
+export class Product extends Model<
+  InferAttributes<Product>,
+  InferCreationAttributes<Product>
+> {
+  declare id: CreationOptional<string>;
+  declare nameProduct: string;
+  declare stock: number;
+  declare price: number;
+  declare user: string;
+}
 
-export default Product;
+Product.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    nameProduct: {
+      type: DataTypes.STRING,
+    },
+    stock: {
+      type: DataTypes.FLOAT,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+    },
+    user: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    tableName: "product",
+  }
+);
